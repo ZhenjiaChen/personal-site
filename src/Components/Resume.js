9f252423d6c0fe62ab8bc4bbc9226831a72d5407
Zhenjia Chen <zhenjia@mit.edu>
2020-03-06 19:37:07 -0500

    Updated experience

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -10,7 +10,7 @@ class Resume extends Component {
         <p>{education.description}</p></div>
       })
       var work = this.props.data.work.map(function(work){
-        return <div key={work.company}><h3>{work.company}</h3>
+        return <div key={work.company}><h3><a href={work.link}>{work.company}</a></h3>
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
             <p>{work.description}</p>
         </div>
@@ -38,7 +38,7 @@ class Resume extends Component {
       <div className="row work">
 
          <div className="three columns header-col">
-            <h1><span>Work</span></h1>
+            <h1><span>Experience</span></h1>
          </div>
 
          <div className="nine columns main-col">
